999f09fa185fba11252ab2959be863bb11882724
NaveenReddy0427 <naveendeva909@gmail.com>
2023-11-28 19:45:24 +0530

    setup the layouts, static files and extract the stylesheets and scripts from subpages to layout

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// setup layouts 
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts)
+
+// setup the static files
+app.use(express.static('./assets'))
+
+// extracting the stylesheets and scripts from subpages to layout 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
 // use express router
 app.use('/', require('./routes/index'))
 
