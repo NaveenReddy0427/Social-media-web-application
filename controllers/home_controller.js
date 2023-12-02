04ba4426b137a519973ebeac5f6412d1d9de01cd
NaveenReddy0427 <naveendeva909@gmail.com>
2023-12-02 09:23:27 +0530

    created schema for comment and sucessfully adding comment to the home page as well

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,16 @@ const Post = require('../models/post')
 module.exports.home = function(req, res){
     // res.cookie('user_id', 25);
 
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         return res.render('home', {
             title: 'codeial | Home',
             posts: posts
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.use('/users', require('./users'))
 // router for post controller
 router.use('/posts', require('./posts'))
 
+// router for comments controller
+router.use('/comments', require('./comments'))
+
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
